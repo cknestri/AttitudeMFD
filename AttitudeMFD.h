93f5b8f07ba2f83ad95fd2e08651f47fb66a502d
Chris Knestrick <Chris.Knestrick@outlook.com>
2018-12-08 10:26:08 -0800

    Fixed MFD2

--- AttitudeMFD.h
+++ AttitudeMFD.h
@@ -50,7 +50,7 @@ const double UPDATE_INTERVAL = 1.0;		// How much time should pass between update
 // For ease of accessing the target list
 #define CurrentTarget (TargetList.GetCurrent()).GetObj()
 
-class AttitudeMFD: public MFD2 {
+class AttitudeMFD: public MFD {
 public:
 
 	AttitudeMFD(DWORD w, DWORD h, VESSEL *vessel);
